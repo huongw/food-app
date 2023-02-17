2d3d5aca7f6987ee33736bb566e2d14fd53faef7
Whitney <designedbywh@gmail.com>
2023-02-16 20:17:56 -0500

    add cuisine routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Category from './components/Category';
 import Pages from './pages/Pages';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Pages/>
+      <BrowserRouter>
+        <Category/>
+        <Pages/>
+      </BrowserRouter>
     </div>
   );
 }
